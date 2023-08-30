adeea01f65b9978e5e8e903a27c949300a4c8f9e
Brett costanzo <127500925+Brettie123@users.noreply.github.com>
2023-08-30 10:25:53 -0300

    Add files via upload
    parte 5 y 6

--- integradorpt1/integrador/integrador/assets/js/cart.js
+++ integradorpt1/integrador/integrador/assets/js/cart.js
@@ -2,6 +2,7 @@
 const btnFinalizarCompra = document.getElementById("btnFinalizarCompra");
 const btnVaciarCarrito = document.getElementById("btnVaciarCarrito");
 const carritoContainer = document.getElementById("carrito-container");
+let insertUser = document.getElementById("user-profile");
 
 cargarProductosCarrito();
 
@@ -111,3 +112,87 @@ function AgregarAlCarrito(producto) {
   localStorage.setItem('AlCarrito', JSON.stringify(carrito));
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let adminLog = JSON.parse(localStorage.getItem("adminLogin"));
+let navLogin = document.getElementById("nav-login");
+
+if (adminLog.admin) {
+  insertUser.innerHTML = `
+                <p id="user-name" style="margin: 0">Hola, Administrador</p>
+                <img src="../images/adminUser.png" id="user-pic">
+                `;
+  navLogin.innerText = "Cerrar Sesión";
+  login.innerHTML = "";
+  login.innerHTML = `
+  <h1>Hola, admin</h1>
+  <button type="submit" class="btn btn-outline-secondary" id="btn-unlog">Cerrar Sesión</button>
+  `;
+  let btnUnlog = document.getElementById("btn-unlog");
+  btnUnlog.onclick = (e) => {
+    e.preventDefault();
+    adminLog.admin = false;
+    localStorage.setItem("adminLogin", JSON.stringify(adminLog));
+    Swal.fire({
+      position: "med",
+      icon: "success",
+      title: "Cerrando Sesion...",
+      showConfirmButton: false,
+      timer: 2000,
+    });
+    setTimeout(function () {
+      window.location.reload();
+    }, 2125);
+  };
+} else {
+  const crearUsuario = () => {
+    let nombreUser = JSON.parse(sessionStorage.getItem("nombreUser"));
+    let apellidoUser = JSON.parse(sessionStorage.getItem("apellidoUser"));
+    let fotoUser = JSON.parse(sessionStorage.getItem("fotoUser"));
+    insertUser.innerHTML = `
+                <p id="user-name" style="margin:0">Hola, ${nombreUser} ${apellidoUser}</p>
+                <img src="${fotoUser}" id="user-pic">
+                `;
+  };
+  if (sessionStorage.getItem("nombreUser") != null) {
+    crearUsuario();
+  } else {
+    fetch("https://randomuser.me/api/")
+      .then((response) => response.json())
+      .then((resultado) => {
+        let apiNombre = resultado.results[0].name.first;
+        let apiApellido = resultado.results[0].name.last;
+        let apiFoto = resultado.results[0].picture.medium;
+        sessionStorage.setItem("nombreUser", JSON.stringify(apiNombre));
+        sessionStorage.setItem("apellidoUser", JSON.stringify(apiApellido));
+        sessionStorage.setItem("fotoUser", JSON.stringify(apiFoto));
+        insertUser.innerHTML = `
+                <p id="user-name" style="margin: 0">Hola, ${apiNombre} ${apiApellido}</p>
+                <img src="${apiFoto}" id="user-pic">
+                `;
+      })
+      .catch(
+        (error) => console.log(error),
+        (insertUser.innerHTML = `
+            <p id="user-name">Cargando usuario...</p>
+            <img src="./assets/img/default-user-img.png" id="user-pic">
+            `)
+      );
+  }
+}
--- integradorpt1/integrador/integrador/assets/js/main.js
+++ integradorpt1/integrador/integrador/assets/js/main.js
@@ -49,6 +49,7 @@ class Producto {
     }
   }
 
+
   function CrearHTMLProducto(producto) {
     const card = document.createElement('div');
     card.className = 'card';
@@ -96,7 +97,7 @@ class Producto {
       });
     } else {
       listaProductos = [
-        { nombre: 'WebShooter"', precio: 80000 , imagen:'https://i.pinimg.com/236x/74/80/02/74800204d0c980381aad055b26e3c8f7.jpg'},
+        { nombre: 'WebShooter"', precio: 80000 , imagen:'https://i.pinimg.com/564x/df/b2/8a/dfb28aa35e860f184557027a1c00305d.jpg'},
         { nombre: 'SpiderMask', precio: 83599, imagen:'https://i.pinimg.com/564x/e4/ca/24/e4ca24d9486cb4fd18132bab86bc702c.jpg'},
         { nombre: 'Hobbie`s guittar', precio: 64000, imagen:'https://i.pinimg.com/236x/27/9a/41/279a41d83073f035cedacebc77c92405.jpg'},
         { nombre: 'miles` jordans', precio: 7300, imagen:'https://i.pinimg.com/236x/73/92/0b/73920b50e9ce48bbf56b1237b97cce73.jpg'},
@@ -110,10 +111,66 @@ class Producto {
   function guardarProductos() {
     localStorage.setItem('productos', JSON.stringify(listaProductos));
   }
-  
+  let insertUser = document.getElementById("user-profile");
   cargarProductos();
   function AgregarAlCarrito(producto) {
     let carrito = JSON.parse(localStorage.getItem('AlCarrito')) || [];
     carrito.push(producto);
     localStorage.setItem('AlCarrito', JSON.stringify(carrito));
+  }
+
+  let SoyAdmin = document.getElementById("vistaAdmin");
+  let vistaañadir = document.getElementById("vistaañadir")
+  let navLogin = document.getElementById("nav-login");
+  let formulario = document.getElementById("form-AgregP");
+  let adminLog = JSON.parse(localStorage.getItem("adminLogin"));
+
+  if (adminLog.admin) {
+    vistaañadir.innerHTML="Añadir un nuevo Producto"
+    SoyAdmin.innerHTML = "";
+    navLogin.innerText = "Cerrar Sesión";
+    insertUser.innerHTML = `
+                  <p id="user-name" style="margin: 0">Hola, Administrador</p>
+                  <img src="./assets/images/adminUser.png" id="user-pic">
+                  `;
+  } else {
+    formulario.style.display = "none";
+  
+    //aca
+    const crearUsuario = () => {
+      let nombreUser = JSON.parse(sessionStorage.getItem("nombreUser"));
+      let apellidoUser = JSON.parse(sessionStorage.getItem("apellidoUser"));
+      let fotoUser = JSON.parse(sessionStorage.getItem("fotoUser"));
+      insertUser.innerHTML = `
+                    <p id="user-name" style="margin: 0">Hola, ${nombreUser} ${apellidoUser}</p>
+                    <img src="${fotoUser}" id="user-pic">
+                    `;
+    };
+    if (sessionStorage.getItem("nombreUser") != null) {
+      crearUsuario();
+    } else {
+      fetch("https://randomuser.me/api/")
+        .then((response) => response.json())
+        .then((resultado) => {
+          // manda perfil a sessionStorage
+          let apiNombre = resultado.results[0].name.first;
+          let apiApellido = resultado.results[0].name.last;
+          let apiFoto = resultado.results[0].picture.medium;
+          sessionStorage.setItem("nombreUser", JSON.stringify(apiNombre));
+          sessionStorage.setItem("apellidoUser", JSON.stringify(apiApellido));
+          sessionStorage.setItem("fotoUser", JSON.stringify(apiFoto));
+          // generar html con API
+          insertUser.innerHTML = `
+                    <p id="user-name" style="margin: 0">Hola, ${apiNombre} ${apiApellido}</p>
+                    <img src="${apiFoto}" id="user-pic">
+                    `;
+        })
+        .catch(
+          (error) => console.log(error),
+          (insertUser.innerHTML = `
+                <p id="user-name">Cargando usuario...</p>
+                <img src="./assets/img/default-user-img.png" id="user-pic">
+                `)
+        );
+    }
   }
